93a0ee7d5201473f636771c6b2a78f4e2f4c5651
danieltongu <daniel.tongu@cwu.edu>
2024-12-07 11:50:03 -0800

    signup with phone

--- app/src/main/java/com/example/chatandroidapp/activities/SignUpWithPhoneActivity.java
+++ app/src/main/java/com/example/chatandroidapp/activities/SignUpWithPhoneActivity.java
@@ -1,4 +1,4 @@
-package com.example.chatandroidapp;
+package com.example.chatandroidapp.activities;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SignupWithPhoneActivity extends AppCompatActivity {
+import com.example.chatandroidapp.R;
+
+public class SignUpWithPhoneActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signup_with_phone);
+        setContentView(R.layout.activity_sign_up_with_phone);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
